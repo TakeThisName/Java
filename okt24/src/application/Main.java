fe69f9804bca759b2e5ecee64922cad2753d3d65
avaaris <avaaris@gmail.com>
2018-10-24 18:52:16 +0200

    Table_R

--- okt24/src/application/Main.java
+++ okt24/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("View.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);// a root után vesszőkkel elválasztva meg lehet adni pixelben a méretet (felűlírja a jfx-et
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- okt24/src/application/ViewController.java
+++ okt24/src/application/ViewController.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{
 
     @FXML
-    private TableView<?> tabla;
+    private TableView<Szemely> tabla;
 
     @FXML
     private TextField mezo_ID;
@@ -23,18 +31,64 @@ public class ViewController {
     private TextField mezo_Szulido;
 
     @FXML
-    void button_Add(ActionEvent event) {
+    private TableView<Szemely> szemely_tabla;
 
+    @FXML
+    private TableColumn<Szemely, String> table_ID;
+
+    @FXML
+    private TableColumn<Szemely, String> table_Veznev;
+
+    @FXML
+    private TableColumn<Szemely, String> table_Knev;
+
+    @FXML
+    private TableColumn<Szemely, String> table_Szilido;
+    
+    @FXML
+    void button_Add(ActionEvent event) {
+    	System.out.println(mezo_ID.getText());
+    	System.out.println(mezo_Veznev.getText());
+    	System.out.println(mezo_Keresztn.getText());
+    	System.out.println(mezo_Szulido.getText());
     }
+    
+    private ObservableList<Szemely> tabla_Adatok = FXCollections.observableArrayList(); // egy üres lista személyekből
 
     @FXML
     void menuitem_Kilepes(ActionEvent event) {
-
+    	Platform.exit();
     }
 
     @FXML
     void menuitem_Nevjegy(ActionEvent event) {
 
     }
+    
+    private void kezdoAdatok() {
+		Szemely sz1 = new Szemely("1", "Pusztai", "Bence", "123");
+		Szemely sz2 = new Szemely("2", "Juhász", "Bence", "456");
+		Szemely sz3 = new Szemely("3", "Kiss", "Balázs", "789");
+		tabla_Adatok.add(sz1);
+		tabla_Adatok.add(sz2);
+		tabla_Adatok.add(sz3);
+	}
+    
+    private void tablaAdatokBeallitasa() {
+    	table_ID.setCellValueFactory(cellData -> cellData.getValue().idProperty());
+    	table_Veznev.setCellValueFactory(cellData -> cellData.getValue().veznevProperty());
+    	table_Knev.setCellValueFactory(cellData -> cellData.getValue().knevProperty());
+    	table_Szilido.setCellValueFactory(cellData -> cellData.getValue().szulidoProperty());
+    	
+    	szemely_tabla.setItems(tabla_Adatok);
+    }
+
+    // implements initializable <- view controllernél
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		kezdoAdatok();
+		tablaAdatokBeallitasa();
+		
+	}
 
 }
