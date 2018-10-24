7fd6b8f987bd80aed48bd0bf3c5234335859613d
avaaris <avaaris@gmail.com>
2018-10-24 16:40:23 +0200

    Tabla_Start

---
+++ okt24/src/application/ViewController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TextField mezo_ID;
+
+    @FXML
+    private TextField mezo_Veznev;
+
+    @FXML
+    private TextField mezo_Keresztn;
+
+    @FXML
+    private TextField mezo_Szulido;
+
+    @FXML
+    void button_Add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuitem_Kilepes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuitem_Nevjegy(ActionEvent event) {
+
+    }
+
+}
